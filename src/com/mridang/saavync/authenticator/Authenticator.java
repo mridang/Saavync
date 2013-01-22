6f82bba7cd7c3d8c138f2d43dfd787c8016d8243
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2013-01-22 18:04:39 +0200

    Modified the Authenticator so that if an account already exists, it doesn't let user add another account of the same type.

--- src/com/mridang/saavync/authenticator/Authenticator.java
+++ src/com/mridang/saavync/authenticator/Authenticator.java
@@ -1,5 +1,7 @@
 package com.mridang.saavync.authenticator;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
@@ -23,9 +25,16 @@ public class Authenticator extends AbstractAccountAuthenticator {
     /* The */
     private final Context ctxContext;
 
+    /*
+     * @see
+     * android.accounts.AbstractAccountAuthenticator#AbstractAccountAuthenticator(
+     * android.content.Context)
+     */
     public Authenticator(Context context) {
+
         super(context);
         ctxContext = context;
+
     }
 
 
@@ -44,7 +53,9 @@ public class Authenticator extends AbstractAccountAuthenticator {
 
 
     /*
-     * @see android.accounts.AbstractAccountAuthenticator#editProperties(android.accounts.AccountAuthenticatorResponse, java.lang.String)
+     * @see android.accounts.AbstractAccountAuthenticator#editProperties(
+     * android.accounts.AccountAuthenticatorResponse
+     * , java.lang.String)
      */
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
@@ -64,12 +75,35 @@ public class Authenticator extends AbstractAccountAuthenticator {
     public Bundle addAccount(AccountAuthenticatorResponse response, String accountType,
             String authTokenType, String[] requiredFeatures, Bundle options) {
 
-        Intent ittIntent = new Intent(ctxContext, AuthenticatorActivity.class);
-        ittIntent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        try {
 
-        Bundle bunResult = new Bundle();
-        bunResult.putParcelable(AccountManager.KEY_INTENT, ittIntent);
-        return bunResult;
+            // If the caller wants to add an account type we don't support, then
+            // return an error
+            if (!accountType.equals(Constants.ACCOUNT_TYPE)) {
+                Bundle bunBundle = new Bundle();
+                bunBundle.putString(AccountManager.KEY_ERROR_MESSAGE,
+                        "Invalid authentication token type requested.");
+                return bunBundle;
+            }
+
+            // Extract the username and password from the Account Manager, and ask
+            // the server for an appropriate AuthToken.
+            AccountManager amrManager = AccountManager.get(ctxContext);
+            Account[] accAccounts = amrManager.getAccountsByType(accountType);
+
+            if (ArrayUtils.isEmpty(accAccounts)) {
+                Intent ittIntent = new Intent(ctxContext, AuthenticatorActivity.class);
+                ittIntent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+                Bundle bunResult = new Bundle();
+                bunResult.putParcelable(AccountManager.KEY_INTENT, ittIntent);
+                return bunResult;
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            return null;
+        }
 
     }
 
@@ -84,28 +118,35 @@ public class Authenticator extends AbstractAccountAuthenticator {
     public Bundle getAuthToken(AccountAuthenticatorResponse aarResponse, Account accAccount,
             String strType, Bundle bunOptions) throws NetworkErrorException {
 
-        // If the caller requested an authToken type we don't support, then
-        // return an error
-        if (!strType.equals(Constants.AUTHTOKEN_TYPE)) {
-            Bundle bunBundle = new Bundle();
-            bunBundle.putString(AccountManager.KEY_ERROR_MESSAGE, "Invalid authentication token type requested.");
-            return bunBundle;
-        }
+        try {
 
-        // Extract the username and password from the Account Manager, and ask
-        // the server for an appropriate AuthToken.
-        AccountManager amrManager = AccountManager.get(ctxContext);
-        String strPassword = amrManager.getPassword(accAccount);
-		String strToken = SaavnClient.authenticate(ctxContext, accAccount.name, strPassword);
-		if (!TextUtils.isEmpty(strToken)) {
-		    Bundle bunResult = new Bundle();
-		    bunResult.putString(AccountManager.KEY_ACCOUNT_NAME, accAccount.name);
-		    bunResult.putString(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNT_TYPE);
-		    bunResult.putString(AccountManager.KEY_AUTHTOKEN, strToken);
-		    return bunResult;
-		}
-		
-		return null;
+            // If the caller requested an authToken type we don't support, then
+            // return an error
+            if (!strType.equals(Constants.AUTHTOKEN_TYPE)) {
+                Bundle bunBundle = new Bundle();
+                bunBundle.putString(AccountManager.KEY_ERROR_MESSAGE,
+                        "Invalid authentication token type requested.");
+                return bunBundle;
+            }
+
+            // Extract the username and password from the Account Manager, and ask
+            // the server for an appropriate AuthToken.
+            AccountManager amrManager = AccountManager.get(ctxContext);
+            String strPassword = amrManager.getPassword(accAccount);
+            String strToken = SaavnClient.authenticate(ctxContext, accAccount.name, strPassword);
+            if (!TextUtils.isEmpty(strToken)) {
+                Bundle bunResult = new Bundle();
+                bunResult.putString(AccountManager.KEY_ACCOUNT_NAME, accAccount.name);
+                bunResult.putString(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNT_TYPE);
+                bunResult.putString(AccountManager.KEY_AUTHTOKEN, strToken);
+                return bunResult;
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            return null;
+        }
 
     }
 
